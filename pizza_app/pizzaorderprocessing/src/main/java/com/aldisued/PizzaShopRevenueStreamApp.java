f00f107bb27a411aea6f06714d37b40b4c568260
leewoobin789 <leewoobin789@gmail.com>
2024-01-09 08:52:07 +0100

    feat: dedup of order before aggregation

--- pizza_app/pizzaorderprocessing/src/main/java/com/aldisued/PizzaShopRevenueStreamApp.java
+++ pizza_app/pizzaorderprocessing/src/main/java/com/aldisued/PizzaShopRevenueStreamApp.java
@@ -5,6 +5,7 @@ import com.aldisued.avro.PizzaOrder;
 import com.aldisued.avro.PizzaOrderShop;
 import com.aldisued.avro.PizzaShopRevenue;
 
+import java.time.Duration;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.Map;
@@ -12,12 +13,20 @@ import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes.IntegerSerde;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.Repartitioned;
+import org.apache.kafka.streams.kstream.SessionWindows;
+import org.apache.kafka.streams.state.SessionStore;
 
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
@@ -43,11 +52,11 @@ public class PizzaShopRevenueStreamApp
                                               final String inputTopic,
                                               final String outputTopic) {
         final Properties streamsConfiguration = new Properties();
-        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "pizza-shop-revenue");
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "pizza-shop");
         streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "pizza-shop-revenue-client");
         streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         streamsConfiguration.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
-        streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+        streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, IntegerSerde.class);
         streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
         streamsConfiguration.put("schema.registry.url", schemaRegistryUrl);
         streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
@@ -55,17 +64,48 @@ public class PizzaShopRevenueStreamApp
         
         final Map<String, String> serdeConfig = Collections.singletonMap("schema.registry.url", schemaRegistryUrl);
 
+        final Serde<PizzaOrder> inputValueSpecificAvroSerde = new SpecificAvroSerde<>();
+        inputValueSpecificAvroSerde.configure(serdeConfig, false);
         final Serde<PizzaOrderShop> keySpecificAvroSerde = new SpecificAvroSerde<>();
         keySpecificAvroSerde.configure(serdeConfig, true); // `true` for record keys
         final Serde<PizzaShopRevenue> valueSpecificAvroSerde = new SpecificAvroSerde<>();
         valueSpecificAvroSerde.configure(serdeConfig, false); // `false` for record values
 
+        /** Start: dedup */
         final StreamsBuilder builder = new StreamsBuilder();
-        final KStream<PizzaOrderShop, PizzaOrder> shopRevenue = builder.stream(inputTopic);
-        final KTable<PizzaOrderShop, PizzaShopRevenue> aggregated = shopRevenue
+        final KStream<PizzaOrderShop, PizzaOrder> input = builder.stream(inputTopic, Consumed.with(keySpecificAvroSerde, inputValueSpecificAvroSerde));
+        Duration gracePeriod = Duration.ofSeconds(2);
+        Duration dedupWindowSize = gracePeriod.multipliedBy(2);
+
+        KStream<PizzaOrderShop, PizzaOrder> deduped = input
+            .selectKey((k,v) -> v.getId())
+            .groupByKey()
+            .windowedBy(SessionWindows.ofInactivityGapAndGrace(dedupWindowSize, gracePeriod))
+            .reduce(
+                (value1, value2) -> value2,
+                Materialized
+                    .with(new IntegerSerde(), inputValueSpecificAvroSerde)
+            )
+           .toStream()
+           .map(
+                (wk, v) -> KeyValue
+                            .pair(PizzaOrderShop.newBuilder().setShop(v.getShop()).build(), v)
+            )
+            .repartition(
+                Repartitioned
+                   .with(keySpecificAvroSerde, inputValueSpecificAvroSerde)
+            );
+        /** End: dedup */
+
+        /** Start: revenue calculation */
+        final KTable<PizzaOrderShop, PizzaShopRevenue> aggregated = deduped
             .groupByKey()
             .aggregate(
-                () -> PizzaShopRevenue.newBuilder().setShop("PLACEHOLDER").setRevenue(0).build(), 
+                () -> PizzaShopRevenue
+                        .newBuilder()
+                        .setShop("PLACEHOLDER")
+                        .setRevenue(0)
+                        .build(), 
                 (k, v, state) -> {
                     int totalPriceOfOrder = 0;
                     Iterator<Pizza> iter = v.getPizzas().iterator();
@@ -78,9 +118,11 @@ public class PizzaShopRevenueStreamApp
                         .setRevenue(state.getRevenue() + totalPriceOfOrder)
                         .build();
                 },
-                Materialized.with(keySpecificAvroSerde, valueSpecificAvroSerde)
+                Materialized
+                    .with(keySpecificAvroSerde, valueSpecificAvroSerde)
             );
         aggregated.toStream().to(outputTopic);
+        /** End: revenue calculation */
         return new KafkaStreams(builder.build(), streamsConfiguration);
     }
 }
